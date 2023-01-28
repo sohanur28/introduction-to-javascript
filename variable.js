278185d42bebdb49be9507f7e4de79099e0d854a
sohanur28 <gsrahman28@gmail.com>
2023-01-29 00:24:24 +0600

    sr

--- variable.js
+++ variable.js
@@ -1,7 +1,19 @@
 // vary + able = variable
+
+// numeric
 var price = 13;
 var age = 23;
 var temperature = 27;
 var weight = 62;
 var money = 10;
 var height = 5;
+
+// string
+var person = "sohan rahman";
+var location = "andork killa bandorbon";
+var special = 'alia bhatt';
+
+//boolean
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
