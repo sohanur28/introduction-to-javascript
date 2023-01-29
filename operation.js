436e5ba32f74d3b26eb055b333083cddbfe0123c
sohanur28 <gsrahman28@gmail.com>
2023-01-29 20:11:53 +0600

    operation

---
+++ operation.js
@@ -0,0 +1,27 @@
+var onionPrice = 20;
+var eggPrice = 10;
+
+// addition, substraction
+var totalPrice = onionPrice + eggPrice;
+var priceDifference = onionPrice - eggPrice;
+
+// console.log(priceDifference);
+
+// console.log(totalPrice);
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+
+
+// multiplication
+var orangePrice = 20;
+var quantity = 7;
+var totalCost = orangePrice * quantity;
+// console.log(totalCost);
+
+
+// division
+var money = 500;
+var player = 10;
+var eacPlayer = money / player;
+console.log(eacPlayer);
